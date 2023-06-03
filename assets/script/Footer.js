6805316f15bc99d597769fbbfe3163444f218e76
LahiruDilshan2000 <UrLahiru2000@gmail.com>
2023-06-03 08:50:42 +0530

    add title

---
+++ assets/script/Footer.js
@@ -0,0 +1,13 @@
+
+$('.content-logos ul li:nth-child(1)').on('click', () => {
+    location.href = "https://web.facebook.com/profile.php?id=100015434130709";
+});
+$('.content-logos ul li:nth-child(2)').on('click', () => {
+    location.href = "https://github.com/LahiruDilshan2000";
+});
+$('.content-logos ul li:nth-child(3)').on('click', () => {
+    location.href = "https://www.linkedin.com/in/lahiru-dilshan-a2091b265/";
+});
+$('.content-logos ul li:nth-child(4)').on('click', () => {
+    location.href = "https://github.com/LahiruDilshan2000";
+});
