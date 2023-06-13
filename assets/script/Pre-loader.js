b87f50415d05357e4fed2aed7280a547bc841bee
LahiruDilshan2000 <UrLahiru2000@gmail.com>
2023-06-13 20:30:40 +0530

    update pre loader

--- assets/script/Pre-loader.js
+++ assets/script/Pre-loader.js
@@ -1,13 +1,17 @@
 let num = 2;
 
 let setTime = () =>{
-    if (num === 4){
+
+    setTimeout(() => {}, 200);
+    $('#num-counter').text(num);
+    num++;
+    /*if (num === 4){
         $('#num-counter').css({fontSize : '0em'});
     }else {
         setTimeout(() => {}, 200);
         $('#num-counter').text(num);
         num++;
-    }
+    }*/
 }
 setInterval(setTime,2000);
 
