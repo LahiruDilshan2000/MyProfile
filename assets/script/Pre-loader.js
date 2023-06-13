b8d5ddfd2df7613217824542a46baaeae4fb40ab
LahiruDilshan2000 <UrLahiru2000@gmail.com>
2023-06-13 19:43:13 +0530

    update number counter

--- assets/script/Pre-loader.js
+++ assets/script/Pre-loader.js
@@ -1,23 +1,20 @@
 let num = 2;
 
 let setTime = () =>{
-    setTimeout(() => {}, 200);
-    $('#num-counter').text(num);
-    num++;
+    if (num === 4){
+        $('#num-counter').css({fontSize : '0em'});
+    }else {
+        setTimeout(() => {}, 200);
+        $('#num-counter').text(num);
+        num++;
+    }
 }
 setInterval(setTime,2000);
 
-$('#num-counter').text()
-$('#section').css({
-    display : "none"
-});
 $(document).ready(() => {
     setTimeout(() => { //delay time after ready document
         $('.pre-loader').css({
             display : "none"
-        });
-        $('#section').css({
-            display : "block"
         });
     },6000);
 });
